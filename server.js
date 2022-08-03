39ac33429c91053349a4e3b02fb232f28787885d
baktash-11 <baktash11@gmail.com>
2022-08-03 01:47:25 -0400

    html page hass been added  using fs method

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ console.log("Welcome to node server js")
 // Set-ExecutionPolicy Unrestricted             <<cammond to un remove restrection for script in windows
 
 const http = require ('http');
-const { url } = require('inspector');
+const fs = require('fs');
+const home= fs.readFileSync('index.html');
 const server = http.createServer((req, res)=>{
     console.log(req.url);
-
-    res.end('Hello Node.js')
+    if(req.url==='/about'){res.end(' all you want to know about us!')}
+    else if(req.url==='/contact'){res.end('we dont have phone # and they blocked out email ;)')}
+    else if(req.url==='/'){res.end(home)}
+    else{
+        res.writeHead(404);
+        res.end('Page was not found')
+    }
+    
 });
 
-server.listen(3000);
+server.listen(3000, ()=>console.log("http://localhost:3000"));
